14f9aff9f7e97879bf6ed9ee668ab063bee0edea
Sergey Shalagin <maloi2000_1@mail.ru>
2023-03-25 16:43:08 +0300

    5

--- temp.py
+++ temp.py
@@ -32,4 +32,37 @@ def menu():
             break
         else:
             print('Введите ещё раз')
-            
+
+def save_dir(dict_phnbk):
+    str_phnbk = ''
+    print(dict_phnbk)
+    for key, value in dict_phnbk.items():
+        str_phnbk += f'{key}:{value} \n'
+    with open('phonebook.txt', 'w') as f:
+        f.write(str_phnbk)
+
+
+def add_cntc(dict_phnbk, new_cntc_in = [0]):
+    if len(new_cntc_in) < 2:
+        name_cntc = input('Введите Имя Фамилию')
+        phone_cntc = input('Введите телефон')
+        comment_cntc = input('Введите комментарий')
+        cntc_list = [phone_cntc, comment_cntc]
+    else:
+        name_cntc, cntc_list = tuple(new_cntc_in)
+    # dict_phnbk[name_cntc] = dict_phnbk.setdefault(name_cntc, cntc_list)
+    dict_phnbk.setdefault(name_cntc, cntc_list)
+    print(f'{name_cntc}, {dict_phnbk[name_cntc]}')
+    return dict_phnbk
+
+
+def cntc_find(dict_phnbk):
+    name_cntc = input('Введите Имя Фамилию')
+    if name_cntc in dict_phnbk:
+        print(f'{name_cntc}: {dict_phnbk[name_cntc]}')
+        return [name_cntc, dict_phnbk[name_cntc]]
+    else:
+        print(f'Не найдено')
+
+
+menu()
